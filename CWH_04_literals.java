21f7ed949bfbdcee3e3889ebfa9e0eaeca2f4c4c
Sujit_0210 <155601038+SujitYalmar@users.noreply.github.com>
2024-08-15 11:17:15 +0530

    Add files via upload

---
+++ CWH_04_literals.java
@@ -0,0 +1,32 @@
+
+public class CWH_04_literals {
+    public static void main(String[] args) {
+        System.out.println("Welcome to World ");
+        double num4=0.232564657411d;
+        char ch = 's';
+        boolean a = true;
+        boolean b = false;
+        short a1 = 45;
+        short a2 = 65;
+        float num3 = 3.546F;
+        float num6 = 5.256F;
+        byte age1 = 24;
+        byte age2 = 65;
+        long num1 = 46454664313L;
+        int num2 =54;
+        int num5 = 65;
+        System.out.println(num5);
+        System.out.println( num3);
+        System.out.println(num6);
+        System.out.println(a2);
+        System.out.println(ch);
+        System.out.println(a);
+        System.out.println(a1);
+        System.out.println(b);
+        System.out.println(num2);
+        System.out.println(num1);
+        System.out.println(age1);
+        System.out.println(num6);
+        System.out.println(age2);
+    }
+}
