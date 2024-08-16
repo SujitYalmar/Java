84549044b5a309397998ca3f75d5cbe1d84335e1
Sujit_0210 <155601038+SujitYalmar@users.noreply.github.com>
2024-08-16 10:49:49 +0530

    Add files via upload

---
+++ cwh_11_ch_01_Exercise_solution.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Scanner;
+
+    public class cwh_11_ch_01_Exercise_solution {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your Physics marks : ");
+        int physics = scan.nextInt();
+        System.out.print("Enter your English marks : ");
+        int English = scan.nextInt();
+        System.out.print("Enter your Chemistry marks : ");
+        int chemistry = scan.nextInt();
+        System.out.print("Enter your Mathematics marks : ");
+        int mathematics = scan.nextInt();
+        System.out.print("Enter your Computer Science marks : ");
+        int computer = scan.nextInt();
+
+        float percentage = ((physics + English + chemistry + mathematics + computer)/500.0f)*100;
+
+        System.out.println("percentage : ");
+        System.out.println(percentage);
+
+
+    }
+}
---
+++ cwh_12_ps_ch2_pro2.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class cwh_12_ps_ch2_pro2 {
+    public static void main(String[] args) {
+        char grade =  'B';
+        //encrypting the grade
+        grade = (char)(grade + 8);
+        System.out.println(grade);
+
+        //decrypting the grade
+        grade = (char)(grade - 8);
+        System.out.println(grade);
+
+    }
+
+}
