cdf0886ef7fd92690270b3cd624312d79a66e5bd
Sujit_650 <155601038+SujitYalmar@users.noreply.github.com>
2024-08-18 18:11:49 +0530

    Add files via upload

---
+++ cwh_31_methods.java
@@ -0,0 +1,21 @@
+package com.company;
+public class cwh_31_methods {
+    static int logic(int x,int y){
+        int z;
+        if (x>y){
+            z = x+y;
+        }
+        else {
+            z = (x+y)*5;
+        }
+        return z;
+    }
+
+    public static void main(String[] args) {
+        int a=5;
+        int b=13;
+        int c;
+        c=logic(a,b);
+        System.out.println(c);
+    }
+}
