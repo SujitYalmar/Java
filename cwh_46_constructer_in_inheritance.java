f7df9d283800fddb2ef965f8f50b57953c024cbf
Sujit_650 <155601038+SujitYalmar@users.noreply.github.com>
2024-08-21 08:14:50 +0530

    Add files via upload

---
+++ cwh_46_constructer_in_inheritance.java
@@ -0,0 +1,26 @@
+package com.company;
+
+class base1{
+    base1(){
+        System.out.println("I am constructer");
+    }
+}
+
+class derived1 extends base1{
+    derived1(){
+        System.out.println("I am derived class contructer");
+    }
+}
+
+class childofderived extends derived1{
+    childofderived(){
+        System.out.println("I am child of derived class constructor");
+    }
+}
+
+
+public class cwh_46_constructer_in_inheritance {
+    public static void main(String[] args) {
+        childofderived c = new childofderived();
+    }
+}
