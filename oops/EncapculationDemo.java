298eed2be9674b076ffa47a3416e9c7c9d4a1800
Sujit_650 <155601038+SujitYalmar@users.noreply.github.com>
2024-10-23 00:25:11 +0530

    Delete oops directory

--- oops/EncapculationDemo.java
+++
@@ -1,9 +0,0 @@
-package oops;
-
-public class EncapculationDemo {
-    public static void main(String[] args) {
-        char arr[]={'a','b','c','d'};
-        String s=new String(arr);
-        System.out.println(s);
-    }
-}
